3e030e41c4819b17f66b3b49dfd771f7e99599cc
Chiyeong Heo <co1504@naver.com>
2022-04-29 11:03:29 +0000

    FIX: training_args.yaml path 수정

--- arguments.py
+++ arguments.py
@@ -10,7 +10,7 @@ def return_arg():
 
     # [model_args, data_args, training_args,_] = parser.parse_args_into_dataclasses(return_remaining_strings=True)
     
-    with open('./training_args.yaml') as f:
+    with open('./configs/training_args.yaml') as f:
         configs = yaml.load(f, Loader=yaml.FullLoader)
     training_arguments, model_arguments, data_arguments = configs['TrainingArguments'], configs['ModelArguments'], configs['DataTrainingArguments']
     model_args = ModelArguments(**model_arguments)
